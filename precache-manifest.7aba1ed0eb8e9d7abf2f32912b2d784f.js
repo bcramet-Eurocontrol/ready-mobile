c39681b7328363196780ee3a1b655c36046d728e
Benjamin Cramet <benjamincramet@MacBook-Pro-de-Benjamin.local>
2023-02-24 20:41:54 +0100

    initial commit

---
+++ precache-manifest.7aba1ed0eb8e9d7abf2f32912b2d784f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "198296a0916437a7b929",
+    "url": "/NM%20Lab/IATA-Apps/css/chunk-vendors.a199ac09.css"
+  },
+  {
+    "revision": "51d3d081dbc9795e762f",
+    "url": "/NM%20Lab/IATA-Apps/css/splashscreen.523e0283.css"
+  },
+  {
+    "revision": "a8022295211b73e247e32c840301f463",
+    "url": "/NM%20Lab/IATA-Apps/img/ec-logo-white.a8022295.svg"
+  },
+  {
+    "revision": "f5e5b3eafb286134d562af97d6c6811c",
+    "url": "/NM%20Lab/IATA-Apps/img/iata-logo.f5e5b3ea.png"
+  },
+  {
+    "revision": "5fbf61e3b478ec14b5784928b8ead0ce",
+    "url": "/NM%20Lab/IATA-Apps/index.html"
+  },
+  {
+    "revision": "ee6a0d104282e6e762b5",
+    "url": "/NM%20Lab/IATA-Apps/js/app.c62bf277.js"
+  },
+  {
+    "revision": "198296a0916437a7b929",
+    "url": "/NM%20Lab/IATA-Apps/js/chunk-vendors.9c777644.js"
+  },
+  {
+    "revision": "51d3d081dbc9795e762f",
+    "url": "/NM%20Lab/IATA-Apps/js/splashscreen.60421df5.js"
+  },
+  {
+    "revision": "0a624d09f34f2fe009d333d571a5b2d5",
+    "url": "/NM%20Lab/IATA-Apps/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/NM%20Lab/IATA-Apps/robots.txt"
+  }
+]);
